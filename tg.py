5e25d0193192a525018f5f56fb7329866abc02cb
nezlobin118@gmail.com <nezlobin118@gmail.com>
2021-11-06 18:33:02 +0800

    При работе с тг идут ошибки, но работает

--- tg.py
+++ tg.py
@@ -8,6 +8,7 @@ from post import main
 import logging
 import traceback
 from core import post
+import asyncio,time
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,27 +23,46 @@ from aiogram.utils import executor
 from post import main
 import logging
 import traceback
+import asyncio
 from core import post
 bot = Bot(token=TOKEN_TG_BOT)
 dp = Dispatcher(bot)
 
-async def on_startup(q):
+async def on_startup():
     while True:
         await bot.send_message(TG_ID, str(post.posts()))
+        break;
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+     asyncio.run(on_startup())
+    #executor.start(future=on_startup)
     """
 else:
-    code = "post.posts()"
+    code = """
+from core import post
+while True:
+    print(post.posts())
+    """
 
-proc = subprocess.run(
+proc = subprocess.Popen(
     ['python3', '-c', code]
 )
 
-#@dp.message_handler()
-#async def error_msg():
-#    await bot.send_message(TG_ID, str(post.posts()))
+@dp.message_handler(commands=['start'])
+async def process_help_command(message: types.Message):
+    proc = subprocess.Popen(
+        ['python3', '-c', code]
+    )
+    await bot.send_message(message.from_user.id, "Старт")
 
-#if __name__ == '__main__':
-#    executor.start_polling(dp)
+@dp.message_handler(commands=['stop'])
+async def process_help_command(message: types.Message):
+    proc.kill()
+    await bot.send_message(message.from_user.id, "Стоп")
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
