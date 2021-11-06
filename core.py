441e38dd381b0ab78a33c896691499ee33983bc7
nezlobin118@gmail.com <nezlobin118@gmail.com>
2021-11-06 17:42:11 +0800

    TG

--- core.py
+++ core.py
@@ -4,13 +4,13 @@ import traceback
 
 logging.basicConfig(level=logging.INFO)
 
-def posts():
-    while True:
-        try:
-            main()
-        except Exception as e:
-            return 'Ошибка:\n' + traceback.format_exc()
-            #print('Ошибка:\n' + traceback.format_exc())
+class post:
+    def posts():
+        while True:
+            try:
+                main()
+            except Exception as e:
+                return 'Ошибка:\n' + traceback.format_exc()
 
-if __name__ == '__main__':
-    posts()
+#if __name__ == '__main__':
+#    post.posts()
--- tg.py
+++ tg.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from config import *
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -5,10 +7,42 @@ from aiogram.utils import executor
 from post import main
 import logging
 import traceback
-from core import posts
+from core import post
 
 logging.basicConfig(level=logging.INFO)
 
 if TOKEN_TG_BOT:
     bot = Bot(token=TOKEN_TG_BOT)
     dp = Dispatcher(bot)
+    code = """
+from config import *
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from post import main
+import logging
+import traceback
+from core import post
+bot = Bot(token=TOKEN_TG_BOT)
+dp = Dispatcher(bot)
+
+async def on_startup(q):
+    while True:
+        await bot.send_message(TG_ID, str(post.posts()))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
+    """
+else:
+    code = "post.posts()"
+
+proc = subprocess.run(
+    ['python3', '-c', code]
+)
+
+#@dp.message_handler()
+#async def error_msg():
+#    await bot.send_message(TG_ID, str(post.posts()))
+
+#if __name__ == '__main__':
+#    executor.start_polling(dp)
